895ca219b02e7947b77a9a3d946c17bcae352b72
xhspdle <xhspdle@naver.com>
2019-04-08 17:50:38 +0900

    에이요

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Portfolio from './Portfolio';
 
 class App extends Component {
   state = {
@@ -13,9 +14,12 @@ class App extends Component {
 
   componentDidMount() {
     // Call our fetch function below once the component mounts
+    /*
     this._callBackendAPI()
       .then(res => this.setState({ data: res.express}))
-      .catch(err => console.error(err));
+      .catch(err => console.error(err));*/
+    this._callBackendAPI()
+      .then(json => this.setState({ data : json}));
     this._callCompanyInfo()
       .then(res => this.setState({companyInfo: {name: res.name, location: res.location}}))
       .catch(err => console.error(err));
@@ -23,6 +27,7 @@ class App extends Component {
 
   // Fetches our GET route from the Express server. 
   // (Note the route we are fetching matches the GET route from server.js)
+  /*
   _callBackendAPI = async () => {
     const response = await fetch('/express_backend');
     const body = await response.json();
@@ -31,8 +36,15 @@ class App extends Component {
       throw Error(body.message);
     }
     return body;
-  };
+  };*/
   
+  _callBackendAPI = async () => {
+    return await fetch('/express_backend')
+      .then(response => response.json())
+      .then(json => json.express)
+      .catch(err => console.error(err));
+  }
+
   _callCompanyInfo = async () => {
     const response = await fetch('/companyInfo');
     const body = await response.json();
@@ -44,12 +56,18 @@ class App extends Component {
   }
 
   _renderPortfolios = () => {
-    // const portfolios = this.state.portfolios.map((portfolios, index) => {
-    //   return <Portfolio title={portfolios.title}/>
-    // });
+     const portfolios = this.state.portfolios.map((portfolios, index) => {
+        return <Portfolio 
+          title={portfolios.title}
+          content={portfolios.content}
+          img={portfolios.img}
+        />
+     });
+     return portfolios;
   }
 
   render() {
+    const { portfolios } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -58,6 +76,7 @@ class App extends Component {
           <p className="App-intro">{this.state.data}</p>
           <p className="App-Company">name: {this.state.companyInfo.name}</p>
           <p className="App-Company">location: {this.state.companyInfo.location}</p>
+          {portfolios ? this._renderPortfolios() : 'Loading...'}
         </header>
       </div>
     );
