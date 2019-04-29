fde703b7a9869763737de39ea66860a51cf6b80f
xhspdle <xhspdle@naver.com>
2019-04-29 15:15:32 +0900

    아직안함

---
+++ src/component/ContactUs.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './ContactUs.scss';
+
+class ContactUs extends Component {
+    
+    state = {}
+
+    render(){
+        return (
+            <div className="ContactUs">
+                <div className="ContactUs-header">
+                    <h2></h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ContactUs;
