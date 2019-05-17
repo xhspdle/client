8cef6260cefa43ac6801cac361fb32ed38f0bd8d
xhspdle <xhspdle@naver.com>
2019-05-17 14:38:58 +0900

    컨택 submit이벤트

--- src/component/ContactUs.js
+++ src/component/ContactUs.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react';
 import './ContactUs.scss';
 
 class ContactUs extends Component {
-    
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            message: e.target.message.value
+        };
+        console.log(data);
+        fetch("/contact/insert", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        }).then((response) => {
+              if(response.status >= 400) {
+                  throw new Error("Bad response from server");
+              }
+              return response.json();
+        }).then(json => console.log(json))
+          .catch(err => console.error(err));
+    }
+
     state = {}
 
     render(){
@@ -17,7 +37,7 @@ class ContactUs extends Component {
                         <img src={require('../static/img/map1.PNG')} alt="map"></img>
                     </div>
                     <div className="ContactUs-column">
-                        <form>
+                        <form onSubmit={(e) => this.handleSubmit(e)}>
                             <label htmlFor="name">Name</label>
                             <input type="text" id="name" name="name" placeholder="Your name.."></input>
                             <label htmlFor="email">Email</label>
